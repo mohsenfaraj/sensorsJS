b452f99a40b49e5e080233c17d19b103daf7e41c
Mohsen Farajollahi <gmohsenfarajollahi@gmail.com>
2023-07-04 10:44:00 +0330

    remoce unnecessary code in sensors.js

--- script.js
+++ script.js
@@ -4,18 +4,10 @@ if (window.DeviceOrientationEvent) {
   window.addEventListener(
     "deviceorientation",
     (event) => {
-      const rotateDegrees = event.alpha; // alpha: rotation around z-axis
-      const leftToRight = event.gamma; // gamma: left to right
-      const frontToBack = event.beta; // beta: front back motion
       cross.style.left = `calc(50% - ${event.gamma * 10}px)`;
       cross.style.top = `calc(50% - ${event.beta * 10}px)`;
       textbox.innerText = `alpha: ${event.alpha}\n Beta: ${event.beta}\n gamma:${event.gamma}`;
-      handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
     },
     true
   );
 }
-
-const handleOrientationEvent = (frontToBack, leftToRight, rotateDegrees) => {
-  // do something amazing
-};
